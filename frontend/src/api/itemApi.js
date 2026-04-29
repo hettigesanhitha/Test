941280f5ad03fefc485933676c4a3e4c63794e5f
hettigesanhitha <hettigesanhitha@gmail.com>
2026-04-29 21:55:42 +0530

    second commit

--- frontend/src/api/itemApi.js
+++ frontend/src/api/itemApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL || "hopeful-heart-production-a122.up.railway.app",
 });
 
 export const getItems = () => API.get("/items");
